a21acac8aea4ed6ceda9006eb307df2850b8a81d
alban <alban@qodlab.io>
2022-06-28 17:25:06 +0200

    Fixture add users

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,48 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //users
+        $user = new User();
+        $user->setEmail('alban@pelissier.dev');
+        $user->setRoles(['user']);
+        $user->setIsVerified(true);
+        $password = $this->hasher->hashPassword($user, 'test1234');
+        $user->setPassword($password);
+        $user->setUsername('userAlban');
+        $user->setLastname('Pelissier');
+        $user->setFirstname('Alban');
 
+        $manager->persist($user);
+        $manager->flush();
+
+        //admin
+        $user = new User();
+        $user->setEmail('alban+admin@pelissier.dev');
+        $user->setRoles(['admin']);
+        $user->setIsVerified(true);
+        $password = $this->hasher->hashPassword($user, 'test1234');
+        $user->setPassword($password);
+        $user->setUsername('adminAlban');
+        $user->setLastname('Pelissier');
+        $user->setFirstname('Alban');
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
